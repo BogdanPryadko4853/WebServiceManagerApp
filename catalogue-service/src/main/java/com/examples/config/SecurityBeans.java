7e39d559f58afe7ba32a6266157e708d1d2e65a5
UseR <147231882+BogdanPryadko4853@users.noreply.github.com>
2024-07-05 00:34:53 +0300

    add security oauth2 resourceServer

--- catalogue-service/src/main/java/com/examples/config/SecurityBeans.java
+++ catalogue-service/src/main/java/com/examples/config/SecurityBeans.java
@@ -1,4 +1,38 @@
 package com.examples.config;
 
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityBeans {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        return http
+                .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+                        .requestMatchers(HttpMethod.POST, "/catalogue-api/products")
+                        .hasAuthority("SCOPE_edit_catalogue")
+                        .requestMatchers(HttpMethod.PATCH, "/catalogue-api/products/{productId:\\d}")
+                        .hasAuthority("SCOPE_edit_catalogue")
+                        .requestMatchers(HttpMethod.DELETE, "/catalogue-api/products/{productId:\\d}")
+                        .hasAuthority("SCOPE_edit_catalogue")
+                        .requestMatchers(HttpMethod.GET)
+                        .hasAuthority("SCOPE_view_catalogue")
+                        .anyRequest().denyAll())
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sessionManagement -> sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer
+                        .jwt(Customizer.withDefaults()))
+                .build();
+    }
+
 }
