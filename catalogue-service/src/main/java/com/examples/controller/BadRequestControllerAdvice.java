17063e83f671e03c25aac673f325a1a3e767788a
UseR <147231882+BogdanPryadko4853@users.noreply.github.com>
2024-07-04 15:33:07 +0300

    BadRequestControllerAdvice

--- catalogue-service/src/main/java/com/examples/controller/BadRequestControllerAdvice.java
+++ catalogue-service/src/main/java/com/examples/controller/BadRequestControllerAdvice.java
@@ -1,8 +1,45 @@
 package com.examples.controller;
 
 
-import org.springframework.stereotype.Controller;
+import lombok.RequiredArgsConstructor;
+import org.apache.coyote.BadRequestException;
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.validation.ObjectError;
 
-@Controller
+import java.util.Locale;
+
+@ControllerAdvice
+@RequiredArgsConstructor
 public class BadRequestControllerAdvice {
+
+
+    private final MessageSource message;
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ProblemDetail> handleBadRequestException(BindException exception,
+                                                                   Locale locale) {
+
+        ProblemDetail problemDetail = ProblemDetail
+                .forStatusAndDetail(HttpStatus.BAD_REQUEST,
+                        this.message.getMessage("errors.404.title"
+                        ,new Object[0],"errors.404.title",
+                                locale));
+
+        problemDetail.setProperty("errors",
+                exception.getAllErrors().stream()
+                        .map(ObjectError::getDefaultMessage)
+                        .toList());
+
+
+    return ResponseEntity.badRequest()
+            .body(problemDetail);
+
+    }
+
 }
