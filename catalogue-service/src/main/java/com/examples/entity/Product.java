510c514cd286c26bade8d7e1759fccbaf6db508f
UseR <147231882+BogdanPryadko4853@users.noreply.github.com>
2024-07-04 14:48:04 +0300

    add Product

---
+++ catalogue-service/src/main/java/com/examples/entity/Product.java
@@ -0,0 +1,29 @@
+package com.examples.entity;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Table(schema = "catalogue",name = "t_product")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "c-title")
+    @NotNull
+    @Size(min = 3, max = 50)
+    private String title;
+
+    @Column(name = "c_details")
+    @Size(max = 1000)
+    private String details;
+}
