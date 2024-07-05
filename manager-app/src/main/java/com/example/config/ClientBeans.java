844e82bbad56f49bccef7e8725882c4cb7ada1c4
UseR <147231882+BogdanPryadko4853@users.noreply.github.com>
2024-07-05 12:01:52 +0300

    add security

--- manager-app/src/main/java/com/example/config/ClientBeans.java
+++ manager-app/src/main/java/com/example/config/ClientBeans.java
@@ -1,4 +1,30 @@
 package com.example.config;
 
+import com.example.client.RestClientProductsRestClient;
+import com.example.security.OAuthClientHttpRequestInterceptor;
+import lombok.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.web.client.RestClient;
+
+@Configuration
 public class ClientBeans {
+
+    @Bean
+    public RestClientProductsRestClient productsRestClient(
+            @Value("${}") String catalogueBaseUri,
+            ClientRegistrationRepository clientRegistrationRepository,
+            OAuth2AuthorizedClientRepository authorizedClientRepository,
+            @Value("${}") String registrationId) {
+        return new RestClientProductsRestClient(RestClient.builder()
+                .baseUrl(catalogueBaseUri)
+                .requestInterceptor(
+                        new OAuthClientHttpRequestInterceptor(
+                                new DefaultOAuth2AuthorizedClientManager(clientRegistrationRepository,
+                                        authorizedClientRepository), registrationId))
+                .build());
+    }
 }
